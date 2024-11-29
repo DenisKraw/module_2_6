fda4a51b278ed00eef11df856281b183c4148768
DenisKraw <Chiks1187@gmail.com>
2024-11-29 22:34:39 +0300

    Add files via upload

---
+++ module_2_6.py
@@ -0,0 +1,12 @@
+age = int(input(' Введите число от 3 до 20 :'))
+result =[]
+for i in range(1,21):
+    if i == age:
+        break
+    for j in range(1,21):
+        if i > j:
+            continue
+        if  age == i + j or age % (i + j) == 0:
+           result.append(i)
+           result.append(j)
+print(*result)
