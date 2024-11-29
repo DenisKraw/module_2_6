c451b4214daec7dafce2a48b02e16b76484c7e81
DenisKraw <Chiks1187@gmail.com>
2024-11-29 22:52:05 +0300

    Add files via upload

--- module_2_6.py
+++ module_2_6.py
@@ -4,7 +4,7 @@ for i in range(1,21):
     if i == age:
         break
     for j in range(1,21):
-        if i > j:
+        if i > j or i + j == 2 or i == j:
             continue
         if  age == i + j or age % (i + j) == 0:
            result.append(i)
